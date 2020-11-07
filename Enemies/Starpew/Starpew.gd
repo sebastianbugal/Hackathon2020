f2d158b80e30b4132ba9e882c89a2ae72d53df4a
Hatbringer <jacobscottgibson@protonmail.com>
2020-11-07 15:19:33 -0800

    Basics of tower combat
    Added enmy tag
    Missiles now seek and destroy enemies

--- Enemies/Starpew/Starpew.gd
+++ Enemies/Starpew/Starpew.gd
@@ -1,2 +1,10 @@
 extends RigidBody2D
 
+signal killed
+
+var health = 50
+
+func _physics_process(delta):
+	if health <= 0:
+		emit_signal("killed", self)
+		queue_free()
--- Missile.gd
+++ Missile.gd
@@ -1,8 +1,14 @@
 extends RigidBody2D
 
 var target
-var thrust = 10
+var thrust = 2
 
 func _physics_process(delta):
 	look_at (target.transform.origin)
 	apply_central_impulse(Vector2(thrust,0).rotated(rotation))
+
+
+func _on_Missile_body_entered(body):
+	if body.is_in_group("Enemy"):
+		body.health -= 25
+		queue_free()
