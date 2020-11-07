dcd6046cd978a3f00528cbe90059c3a2268669d5
sebastianbugal <49225786+sebastianbugal@users.noreply.github.com>
2020-11-07 12:31:19 -0800

    pls work pls

--- lord.gd
+++ lord.gd
@@ -6,4 +6,6 @@ func _physics_process(delta):
 	if Input.is_action_pressed("left"):
 		apply_torque_impulse(-100)
 	if Input.is_action_pressed("forward"):
+		apply_central_impulse(Vector2(0,10).rotated(rotation))
+	if Input.is_action_pressed("backward"):
 		apply_central_impulse(Vector2(0,-10).rotated(rotation))
