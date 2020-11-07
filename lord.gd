8d279d1515aa6e33cfec3d8794f5f1cc9b6c5dc9
Hatbringer <jacobscottgibson@protonmail.com>
2020-11-07 12:04:01 -0800

    Player is now a rigidbody ship

--- lord.gd
+++ lord.gd
@@ -1,28 +1,7 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-var run_speed = 60
-var jump_strength = 360
-var gravity = 10
-
-var velocity = Vector2(0,0)
-
-func _physics_process(_delta):
-	#HORIZONTAL MOVEMENT
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += run_speed
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= run_speed
-	#FRICTION
-	velocity.x /= 1.2
-
-	#JUMPING remove the "just" if you want to hold to jump
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = -jump_strength
-	#JUMP CUTOFF
-	if Input.is_action_just_released("ui_up") and velocity.y < 0:
-		velocity.y /= 2
-	
-	#GRAVITY
-	velocity.y += gravity
-	
-	velocity = move_and_slide(velocity, Vector2.UP)
+func _physics_process(delta):
+	if Input.is_action_pressed("right"):
+		apply_torque_impulse(100)
+	if Input.is_action_pressed("left"):
+		apply_torque_impulse(-100)
