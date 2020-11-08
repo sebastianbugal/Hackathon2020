a9c3618dc277981ffab34c5cbde866bd7f333c73
sebastianbugal <49225786+sebastianbugal@users.noreply.github.com>
2020-11-07 16:29:17 -0800

    Path

--- App.gd
+++ App.gd
@@ -3,12 +3,13 @@ extends Node
 onready var nav_2d : Navigation2D = $Navigation2D
 onready var line_2d : Line2D = $Line2D
 onready var enemy : RigidBody2D = $Starpew
+onready var lord : RigidBody2D = $lord
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not event is InputEventMouseButton:
 		return 
 	if event.button_index != BUTTON_LEFT or not event.pressed:
 		return
-	var new_path : = nav_2d.get_simple_path(enemy.global_position, event.global_position)
+	var new_path : = nav_2d.get_simple_path(enemy.position, event.global_position - Vector2(1920/2,1080/2))
 	line_2d.points = new_path
 	enemy.path = new_path
--- Enemies/Starpew/Starpew.gd
+++ Enemies/Starpew/Starpew.gd
@@ -10,35 +10,32 @@ var path : = PoolVector2Array() setget set_path
 func _ready() -> void:
 	set_process(false)
 	
-func _process(delta: float) -> void:
-	var move_distance : = 400*delta
-	move_along_path(move_distance)
-
-func move_along_path(distance : float) -> void:
-	var start_point : = position
-	for i in range(path.size()):
-		var distance_to_next : = start_point.distance_to(path[0])
-		if distance <= distance_to_next and distance >= 0.0:
-			position = start_point.linear_interpolate(path[0], distance/distance_to_next)
-			break
-		elif distance < 0.0:
-			position = path[0]
-			set_process(false)
-			break
-		distance -= distance_to_next
-		start_point = path[0]
-		path.remove(0)
-
-func set_path(value : PoolVector2Array) -> void:
-	path = value
-	if value.size() == 0:
-		return
-	set_process(true)
-
 func _physics_process(delta):
+	move_along_path()
 	if health <= 0:
 		emit_signal("killed", self)
 		var e = exploision.instance()
 		e.transform = transform
 		get_tree().current_scene.add_child(e)
 		queue_free()
+
+
+func move_along_path() -> void:
+	var start_point : = position
+	for i in range(path.size()):
+		var distance_to_next : = start_point.distance_to(path[0])
+		if 20<= distance_to_next:
+			look_at(path[0])
+			apply_central_impulse(Vector2(1,0).rotated(rotation))
+			# position = start_point.linear_interpolate(path[0], distance/distance_to_next)
+			break
+		start_point = path[0]
+		path.remove(0)
+
+	
+func set_path(value : PoolVector2Array) -> void:
+	path = value
+	if value.size() == 0:
+		return
+	set_process(true)
+
