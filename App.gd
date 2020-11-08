8f22d8b400aaf8bb8d331d0eee9fa733bf2fc054
sebastianbugal <49225786+sebastianbugal@users.noreply.github.com>
2020-11-07 18:39:28 -0800

    Core stuff

--- App.gd
+++ App.gd
@@ -2,29 +2,33 @@ extends Node
 
 onready var nav_2d : Navigation2D = $Navigation2D
 onready var line_2d : Line2D = $Line2D
-onready var enemy : RigidBody2D = $Starpew
+
 onready var core : RigidBody2D = $Core
 var wave_size = 5
 onready var enem = preload("res://Enemies/Starpew/Starpew.tscn")
 var timer
 
-func _unhandled_input(event: InputEvent) -> void:
-	if not event is InputEventMouseButton:
-		return 
-	if event.button_index != BUTTON_LEFT or not event.pressed:
-		return
-	var new_path : = nav_2d.get_simple_path(enemy.position, core.position)
-	line_2d.points = new_path
-	enemy.path = new_path
-	
-	
-
 func _on_Core_body_entered(body):
 	pass # Replace with function body.
 
-
 func _on_WaveCounter_timeout():
 	for i in range(wave_size):
-		var cur = enem.instance()
-		cur.position = Vector2(-650,-650)
+		var side = i%4
+		var cur
+		if(side == 0):
+			cur = enem.instance()
+			cur.position = Vector2(-650,-650)
+		elif side == 1:
+			cur = enem.instance()
+			cur.position = Vector2(650,650)
+		elif side == 2:
+			cur = enem.instance()
+			cur.position = Vector2(-650,650)
+		elif side == 3:
+			cur = enem.instance()
+			cur.position = Vector2(650,-650)
 		get_tree().current_scene.add_child(cur)
+		var new_path : = nav_2d.get_simple_path(cur.position, core.position)
+		cur.path = new_path
+	wave_size+=3
+	
--- lord.gd
+++ lord.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 var Energy = 0
 var GettingEnergy= false
 onready var tower = preload("res://Towers/BasicLauncher.tscn")
+onready var mc = preload("res://Towers/MachineGun.tscn")
 func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		apply_torque_impulse(50)
@@ -15,6 +16,11 @@ func _physics_process(delta):
 		Energy+=.01
 	if Input.is_action_just_pressed("DropTower") and Energy >= 10:
 		var cur = tower.instance()
+		cur.position = position
+		get_tree().current_scene.add_child(cur)
+		Energy -= 10
+	if Input.is_action_just_pressed("DropTower2") and Energy >= 10:
+		var cur = mc.instance()
 		cur.position = position
 		get_tree().current_scene.add_child(cur)
 		Energy -= 10
