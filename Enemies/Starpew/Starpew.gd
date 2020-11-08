7ea66680b80e7c820d2708712573be07b344306e
Hatbringer <jacobscottgibson@protonmail.com>
2020-11-07 17:38:56 -0800

    new enemy and new tower

--- Enemies/Starpew/Starpew.gd
+++ Enemies/Starpew/Starpew.gd
@@ -4,7 +4,8 @@ onready var exploision = preload("res://Explosion.tscn")
 
 signal killed
 
-var health = 50
+export var speed = 20
+export var health = 50
 var path : = PoolVector2Array() setget set_path
 
 func _ready() -> void:
@@ -26,7 +27,7 @@ func move_along_path() -> void:
 		var distance_to_next : = start_point.distance_to(path[0])
 		if 20<= distance_to_next:
 			look_at(path[0])
-			apply_central_impulse(Vector2(20,0).rotated(rotation))
+			apply_central_impulse(Vector2(speed,0).rotated(rotation))
 			# position = start_point.linear_interpolate(path[0], distance/distance_to_next)
 			break
 		start_point = path[0]
---
+++ Towers/Bullet.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+var target
+var speed = 512
+var linear_velocity
+
+func _ready():
+	look_at(target.position)
+	linear_velocity = Vector2(speed,0).rotated(rotation)
+
+func _physics_process(delta):
+	linear_velocity = move_and_slide(linear_velocity)
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Enemy"):
+		body.health -= 3
+		queue_free()
+
