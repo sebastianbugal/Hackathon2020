f3c824ea834e872b70a8150a30c0a93decf6a431
sebastianbugal <49225786+sebastianbugal@users.noreply.github.com>
2020-11-07 17:46:04 -0800

    Spawning

--- App.gd
+++ App.gd
@@ -4,6 +4,9 @@ onready var nav_2d : Navigation2D = $Navigation2D
 onready var line_2d : Line2D = $Line2D
 onready var enemy : RigidBody2D = $Starpew
 onready var core : RigidBody2D = $Core
+var wave_size = 5
+onready var enem = preload("res://Enemies/Starpew/Starpew.tscn")
+var timer
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not event is InputEventMouseButton:
@@ -13,7 +16,15 @@ func _unhandled_input(event: InputEvent) -> void:
 	var new_path : = nav_2d.get_simple_path(enemy.position, core.position)
 	line_2d.points = new_path
 	enemy.path = new_path
-
+	
+	
 
 func _on_Core_body_entered(body):
 	pass # Replace with function body.
+
+
+func _on_WaveCounter_timeout():
+	for i in range(wave_size):
+		var cur = enem.instance()
+		cur.position = Vector2(-650,-650)
+		get_tree().current_scene.add_child(cur)
--- lord.gd
+++ lord.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 var Energy = 0
 var GettingEnergy= false
+onready var tower = preload("res://Towers/BasicLauncher.tscn")
 func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		apply_torque_impulse(50)
@@ -12,3 +13,9 @@ func _physics_process(delta):
 		apply_central_impulse(Vector2(0,-5).rotated(rotation))
 	if GettingEnergy:
 		Energy+=.01
+	if Input.is_action_just_pressed("DropTower") and Energy >= 10:
+		var cur = tower.instance()
+		cur.position = position
+		get_tree().current_scene.add_child(cur)
+		Energy -= 10
+	print(Energy)
